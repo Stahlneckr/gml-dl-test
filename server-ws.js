07da22f70b9463fe9d21bcc384961fe9b1f0ecdc
Nick Stahlnecker <stahlnecker@hey.com>
2022-04-29 13:14:04 -0400

    agian1

--- server-ws.js
+++ server-ws.js
@@ -1,40 +1,16 @@
-import { createServer } from 'http';
-import { parse } from 'url';
 import { WebSocketServer } from 'ws';
 
-// import express from 'express';
-// const app = express();
-// const port = 3001;
-
-// app.get("/", (req, res) => res.type('html').send(html));
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-const server = createServer();
-const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', function connection(ws) {
   console.log('\nNew client connected');
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log(`received: ${data}`);
+    ws.send(`echo: ${data}`);
   });
 
-  ws.send('something');
+  setTimeout(() => ws.send('You are connected!'), 1000);
 });
 
-wss.on('error', (e) => console.log(e))
-
-server.on('upgrade', function upgrade(request, socket, head) {
-  const { pathname } = parse(request.url);
-
-  if (pathname === '/ws') {
-    wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request);
-    });
-  } else {
-    socket.destroy();
-  }
-});
-
-server.listen(3001);
+wss.on('listening', () => { console.log('WSS listening at :3001') });
