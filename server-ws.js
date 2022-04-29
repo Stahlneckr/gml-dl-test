f70074b08b30afb95ad115d7a6750e89e80dd8e7
Nick Stahlnecker <stahlnecker@hey.com>
2022-04-29 11:50:00 -0400

    express

--- server-ws.js
+++ server-ws.js
@@ -1,15 +1,23 @@
-import { WebSocketServer } from 'ws';
+// import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 80 });
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-wss.on('connection', function connection(ws) {
-  console.log('\nNew client connected');
+app.get("/", (req, res) => res.type('html').send(html));
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-  ws.send('something');
-});
+// const wss = new WebSocketServer({ port: 80 });
 
-wss.on('listening', () => { console.log('WSS listening at :80') });
+// wss.on('connection', function connection(ws) {
+//   console.log('\nNew client connected');
+
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
+
+//   ws.send('something');
+// });
+
+// wss.on('listening', () => { console.log('WSS listening at :80') });
